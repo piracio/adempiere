21e6f280294a7e31088d3dda7d6a46c54cc2bac6
Horacio Miranda <hmiranda@prolinux.cl>
2025-05-19 07:10:46 +1200

    Keylistener added as addon

--- zkwebui/js/zul/keylistener/keylistener.js
+++ zkwebui/js/zul/keylistener/keylistener.js
@@ -92,5 +92,11 @@
             }
         });
 
+	zul.KeyListener.molds = {
+	        default: function (out) {
+	        out.push('<div', this.domAttrs_(), '></div>');
+	    }
+        };
+
     });
 })();
